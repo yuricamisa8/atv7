070496111e9e40f1009cea24d1d8e1f73affaab1
yuricamisa8 <francisco.cardoso94@aluno.ce.gov.br>
2024-09-09 09:01:35 -0300

    imposto

---
+++ atv7.py
@@ -0,0 +1,5 @@
+# Enunciado: Uma loja aplica um imposto de 12% sobre o valor dos produtos. Crie um programa que receba o preço de um produto e calcule o preço final com o imposto incluído
+produto = float(input("receba o preço do produto"))
+porcentagem = 1.12
+soma = produto * porcentagem
+print(soma)
